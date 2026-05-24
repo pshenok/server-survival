a101b9ee743374c923e0f260a82e029641fd89b6
kp <pshenok.dev@gmail.com>
2026-05-24 11:09:48 -0700

    style(campaign): match project 4-space indentation

--- src/campaign/diagram.js
+++ src/campaign/diagram.js
@@ -1,5 +1,5 @@
 // SVG architecture diagram generator for Briefing modal.
 // Renders a horizontal flow of service icons from a level's preBuilt array.
 function renderArchitectureSVG(preBuilt, highlights) {
-  return '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="40"></svg>';
+    return '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="40"></svg>';
 }
